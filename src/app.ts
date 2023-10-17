c01e36bace02dc989889e84631c0175baddf37c3
hohihohi <a56.tazima@gmail.com>
2023-10-18 01:02:11 +0900

    fix: app.test.ts
    test getEnvParams and fix tweaks to pass its test

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,29 @@
 import * as aws_cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { managementParameter } from "./parameter";
+import { EnvParameter, managementParameter } from "./parameter";
 
 // App
 const app = new aws_cdk.App();
 aws_cdk.Tags.of(app).add("App", "bela-poc");
 
-// Extract deploy environment names from context
-// const env_arg: string[] = `${app.node.tryGetContext("env_names")}`.split(",");
-const env_arg: string[] = `${app.node.tryGetContext("env_names")}`.split(",");
-const envNames = env_arg.length > 0 ? env_arg : [managementParameter.envName];
-const allParams = [managementParameter];
-const envParams = allParams.filter((param) => envNames.includes(param.envName));
+// Extract deploy environment parameters from context
+export function getEnvParams(ap: aws_cdk.App): EnvParameter[] {
+  const ALL_PARAMS = [managementParameter];
+
+  const env_arg: string[] = ap.node.tryGetContext("env_names")
+    ? String(ap.node.tryGetContext("env_names")).split(",")
+    : [];
+  const envNames = env_arg.length > 0 ? env_arg : [managementParameter.envName];
+  const envParams = ALL_PARAMS.filter((param) =>
+    envNames.includes(param.envName),
+  );
+  if (envParams.length <= 0) {
+    throw new RangeError(
+      "No environment parameters are found. Please confirm env_names",
+    );
+  }
+  return envParams;
+}
 
 // Stack
 export class MyStack extends aws_cdk.Stack {
@@ -21,6 +33,8 @@ export class MyStack extends aws_cdk.Stack {
     // define resources here...
   }
 }
+
+const envParams = getEnvParams(app);
 const mystack = new MyStack(app, "bela-poc-dev", ...envParams);
 aws_cdk.Tags.of(mystack).add("Stack", "bela-poc-dev");
 
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,45 @@
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { MyStack } from "../src/app";
+import { getEnvParams, MyStack } from "../src/app";
+import { managementParameter } from "../src/parameter";
 
+// UNIT TEST
+describe("getEnvParams", () => {
+  it("should return environment parameters when no parameter are specified", () => {
+    // arrange
+    const app = new App();
+    const expected = [managementParameter];
+    // act
+    const envParams = getEnvParams(app);
+    // assert
+    expect(envParams).toHaveLength(1);
+    expect(envParams[0]).toEqual(expected[0]);
+  });
+  it("should return environment parameters when valid arguments are specified", () => {
+    // arrange
+    // NOTE: https://github.com/aws/aws-cdk/issues/5149
+    const app = new App({ context: { env_names: "Management" } });
+    const expected = [managementParameter];
+    // act
+    const envParams = getEnvParams(app);
+    // assert
+    expect(envParams).toHaveLength(1);
+    expect(envParams[0]).toEqual(expected[0]);
+  });
+  it("should throw error when no environment parameters are found", () => {
+    // arrange
+    // NOTE: https://github.com/aws/aws-cdk/issues/5149
+    const app = new App({ context: { env_names: "Dev" } });
+    // act & assert
+    expect(() => getEnvParams(app)).toThrowError(
+      new RangeError(
+        "No environment parameters are found. Please confirm env_names",
+      ),
+    );
+  });
+});
+
+// SNAPSHOT TEST
 test("Snapshot", () => {
   const app = new App();
   const stack = new MyStack(app, "test");
